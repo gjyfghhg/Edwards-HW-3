11461ccc0a1d5e99c2a9c761f0c5c52251c433f1
Edward <70453612+gjyfghhg@users.noreply.github.com>
2020-09-18 21:14:10 -0400

    Add files via upload

---
+++ hw-3.js
@@ -0,0 +1,55 @@
+const isString("future sphere") = (input) => {
+  if (typeof input === "string") {
+    return true;
+  } else {
+    return false;
+  }
+};
+str.isString("Future Sphere"); //?
+str.isString("35"); //?
+
+const isBlank("") = (input) => {
+  if (typeof input === "blank") {
+    return true;
+  } else {
+    return false;
+  }
+};//?
+str.isBlank;""; //?
+str.isBlank;"peter"; //?
+
+const truncateString("robin hood", 4) = (input) => {
+  if (typeof input === "blank") {
+    return true;
+  } else {
+    return false;
+  }
+};//?
+str.truncateString = "Robin Hood"4 ; //?
+
+const stringParametize("robin Hood from china") = (input) => {
+    if (typeof input === "blank") {
+      return true;
+    } else {
+      return false;
+    }
+  };//?
+str.stringParametize = "United States of America"//?
+
+const camelize("Javascript Excersizes") = (input) => {
+    if (typeof input === "blank") {
+      return true;
+    } else {
+      return false;
+    }
+  };//?
+
+
+  const Randomize("Javascript Excersizes") = (input) => {
+    if (typeof input === "blank") {
+      return true;
+    } else {
+      return false;
+    }
+  };//?
+  str.Randomize("10,15")//?
